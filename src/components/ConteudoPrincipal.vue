1c1d7512972325f07987e94eb811cbbc715f563d
Natanael A. de Morais <natanaelallves@gmail.com>
2024-10-22 05:36:42 -0300

    Vinculando dados com v-bind | Rederizando condicionalmente

--- src/components/ConteudoPrincipal.vue
+++ src/components/ConteudoPrincipal.vue
@@ -16,11 +16,16 @@
                 Sua lista:
             </span>
 
-            <ul class="ingredientes-sua-lista">
-                <li v-for="ingrediente in ingredientes" class="ingrediente">
+            <ul v-if="ingredientes.length" class="ingredientes-sua-lista">
+                <li v-for="ingrediente in ingredientes" :key="ingrediente" class="ingrediente">
                     {{ ingrediente }}
                 </li>
             </ul>
+
+            <p v-else class="paragrafo list-vazia">
+                <img src="../assets/images/icones/lista-vazia.svg" alt="Ícone de pesquisa">
+                Sua lista está vazia, selecione ingredientes para iniciar.
+            </p>
         </section>
     </main>
 </template>
