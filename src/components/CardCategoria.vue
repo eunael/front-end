a8dd7c83e3afd911a76e37ad864c915118ea73dd
Natanael A. de Morais <natanaelallves@gmail.com>
2024-10-22 20:23:51 -0300

    Controlando visual com estado

--- src/components/CardCategoria.vue
+++ src/components/CardCategoria.vue
@@ -1,13 +1,13 @@
 <script lang="ts">
 import type { ICategoria } from '@/interfaces/ICategoria';
 import type { PropType } from 'vue';
-import Tag from './Tag.vue';
+import IngredienteSelecionavel from './IngredienteSelecionavel.vue';
 
 export default {
     props: {
         categoria: {type: Object as PropType<ICategoria>, required: true}
     },
-    components: {Tag}
+    components: {IngredienteSelecionavel}
 }
 </script>
 
@@ -21,7 +21,7 @@ export default {
 
         <ul class="categoria__ingredientes">
             <li v-for="ingrediente in categoria.ingredientes" :key="ingrediente">
-                <Tag :texto="ingrediente" />
+                <IngredienteSelecionavel :ingrediente="ingrediente" />
             </li>
         </ul>
     </article>
