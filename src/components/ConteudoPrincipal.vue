5bed1ea31d63029e3e6e8ecebaaf876dfc2ece7b
Natanael A. de Morais <natanaelallves@gmail.com>
2024-10-22 18:40:21 -0300

    Personalizando estilos

--- src/components/ConteudoPrincipal.vue
+++ src/components/ConteudoPrincipal.vue
@@ -22,7 +22,7 @@ export default {
 
             <ul v-if="ingredientes.length" class="ingredientes-sua-lista">
                 <li v-for="ingrediente in ingredientes" :key="ingrediente" class="ingrediente">
-                    <Tag  :texto="ingrediente"/>
+                    <Tag  :texto="ingrediente" :ativa="true"/>
                 </li>
             </ul>
 
